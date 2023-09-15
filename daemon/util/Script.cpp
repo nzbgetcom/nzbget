f25a87c094872b582c4ab8e97a6b241efeeea292
ureyNZB <139906999+ureyNZB@users.noreply.github.com>
2023-09-15 13:23:44 +0300

    fix python 3.x script execution windows support (#8)
    * fix python 3.x script execution windows support
    
    python 3.x has %L (unprocessable) in command string format windows registry key instead of %1(processable) in 2.x python. So we can just check and replace.
    
    * fix for Debug configuration

--- daemon/util/Script.cpp
+++ daemon/util/Script.cpp
@@ -282,10 +282,11 @@ void ScriptController::PrepareArgs()
 					nullptr, command, &bufLen))
 				{
 					command[bufLen] = '\0';
-					debug("Command: %s", command);
-
+					CString scommand(command);
+					scommand.Replace("%L", "%1");// Python 3.x has %L in command instead of %1
+					debug("Command: %s", scommand.Str());
 					DWORD_PTR args[] = {(DWORD_PTR)*m_args[0], (DWORD_PTR)0};
-					if (FormatMessage(FORMAT_MESSAGE_FROM_STRING | FORMAT_MESSAGE_ARGUMENT_ARRAY, command, 0, 0,
+					if (FormatMessage(FORMAT_MESSAGE_FROM_STRING | FORMAT_MESSAGE_ARGUMENT_ARRAY, scommand, 0, 0,
 						m_cmdLine, sizeof(m_cmdLine), (va_list*)args))
 					{
 						Util::TrimRight(Util::ReduceStr(m_cmdLine, "*", ""));
