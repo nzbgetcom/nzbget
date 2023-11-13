4ecdf992bcd177a02b4fb70cfaeab17b1a9d0a22
dnzbk <denis@nzbget.com>
2023-11-13 14:50:41 +0300

    Add: parse options, commands, task

--- daemon/extension/ManifestFile.cpp
+++ daemon/extension/ManifestFile.cpp
@@ -25,19 +25,19 @@
 
 namespace ManifestFile
 {
-	const char* MANIFEST_FILE = "manifest.json";
+	const char *MANIFEST_FILE = "manifest.json";
 
 	bool Load(Manifest& manifest, const char* directory)
 	{
 		DirBrowser dir(directory);
-		while (const char* filename = dir.Next()) 
+		while (const char* filename = dir.Next())
 		{
 			if (strncmp(filename, MANIFEST_FILE, sizeof(MANIFEST_FILE)) == 0)
 			{
 				BString<1024> fullFilename("%s%c%s", directory, PATH_SEPARATOR, MANIFEST_FILE);
 				std::fstream fs(fullFilename);
 				Json::error_code ec;
-				
+
 				Json::JSON json = Json::Read(fs, ec);
 				if (ec)
 					return false;
@@ -50,6 +50,33 @@ namespace ManifestFile
 				manifest.displayName = json.at("displayName").as_string().c_str();
 				manifest.kind = json.at("kind").as_string().c_str();
 				manifest.license = json.at("license").as_string().c_str();
+				manifest.task = json.at("task").as_string().c_str();
+
+				for (auto& value : json.at("options").as_array())
+				{
+					Json::object object = value.as_object();
+					Option option;
+					option.name = object.at("name").as_string().c_str();
+					option.description = object.at("description").as_string().c_str();
+					option.displayName = object.at("displayName").as_string().c_str();
+					option.value = object.at("value").as_string().c_str();
+					for (auto& value : object.at("select").as_array())
+					{
+						option.select.push_back(value.as_string().c_str());
+					}
+					manifest.options.push_back(std::move(option));
+				}
+
+				for (auto& value : json.at("commands").as_array())
+				{
+					Json::object object = value.as_object();
+					Command command;
+					command.name = object.at("name").as_string().c_str();
+					command.description = object.at("description").as_string().c_str();
+					command.action = object.at("action").as_string().c_str();
+					command.displayName = object.at("displayName").as_string().c_str();
+					manifest.commands.push_back(std::move(command));
+				}
 				return true;
 			}
 		}
