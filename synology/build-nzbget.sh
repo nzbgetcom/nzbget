c677823ebeeeef3629556185e8c89e340793d286
phnzb <pavel@nzbget.com>
2023-12-01 19:48:32 +0000

    Decrease concurrency. Fix cleanup.

--- synology/build-nzbget.sh
+++ synology/build-nzbget.sh
@@ -33,7 +33,7 @@ VERSION=$(date '+%Y%m%d')-$VERSION
 sed -e "s|version=.*$|version=\"$VERSION\"|g" -i INFO.sh
 
 # build
-/toolkit/pkgscripts-ng/PkgCreate.py -v 7.0 -c nzbget $BUILD_PARAM
+/toolkit/pkgscripts-ng/PkgCreate.py -v 7.0 -c -P 2 nzbget $BUILD_PARAM
 
 # remove debug packages and set user perms on packages
 mv /toolkit/result_spk/nzbget-$VERSION/ /toolkit/result_spk/nzbget/
