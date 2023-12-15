d1ea3920119aad8901efb61ba2009da26bdf6d00
dnzbk <denis@nzbget.com>
2023-12-15 10:10:34 +0300

    Fix: wrong guard name

--- daemon/util/ScriptController.h
+++ daemon/util/ScriptController.h
@@ -14,12 +14,12 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 
-#ifndef SCRIPT_H
-#define SCRIPT_H
+#ifndef SCRIPTCONTROLLER_H
+#define SCRIPTCONTROLLER_H
 
 #include "NString.h"
 #include "Container.h"
