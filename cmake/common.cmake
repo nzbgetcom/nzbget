33b34508ba289e4acb23c5c6b8c0612f2c0de1ef
dnzbk <denis@nzbget.com>
2024-02-15 16:19:40 +0300

    try to fix simd

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -41,9 +41,115 @@ find_package(Boost REQUIRED COMPONENTS json)
 include_directories(${CMAKE_BINARY_DIR})
 include_directories(${CMAKE_SOURCE_DIR})
 
-add_subdirectory(${CMAKE_SOURCE_DIR}/daemon)
 add_subdirectory(${CMAKE_SOURCE_DIR}/lib)
 
+set(SRC
+	${CMAKE_SOURCE_DIR}/daemon/connect/Connection.cpp
+	${CMAKE_SOURCE_DIR}/daemon/connect/TlsSocket.cpp
+	${CMAKE_SOURCE_DIR}/daemon/connect/WebDownloader.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/extension/CommandScript.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/FeedScript.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/NzbScript.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/PostScript.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/PostScript.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/QueueScript.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/ScanScript.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/SchedulerScript.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/ScriptConfig.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/ScriptConfig.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/Extension.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/ExtensionLoader.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/ExtensionManager.cpp
+	${CMAKE_SOURCE_DIR}/daemon/extension/ManifestFile.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/feed/FeedCoordinator.cpp
+	${CMAKE_SOURCE_DIR}/daemon/feed/FeedFile.cpp
+	${CMAKE_SOURCE_DIR}/daemon/feed/FeedFilter.cpp
+	${CMAKE_SOURCE_DIR}/daemon/feed/FeedInfo.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/frontend/ColoredFrontend.cpp
+	${CMAKE_SOURCE_DIR}/daemon/frontend/Frontend.cpp
+	${CMAKE_SOURCE_DIR}/daemon/frontend/LoggableFrontend.cpp
+	${CMAKE_SOURCE_DIR}/daemon/frontend/NCursesFrontend.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/main/CommandLineParser.cpp
+	${CMAKE_SOURCE_DIR}/daemon/main/DiskService.cpp
+	${CMAKE_SOURCE_DIR}/daemon/main/Maintenance.cpp
+	${CMAKE_SOURCE_DIR}/daemon/main/nzbget.cpp
+	${CMAKE_SOURCE_DIR}/daemon/main/Options.cpp
+	${CMAKE_SOURCE_DIR}/daemon/main/Scheduler.cpp
+	${CMAKE_SOURCE_DIR}/daemon/main/StackTrace.cpp
+	${CMAKE_SOURCE_DIR}/daemon/main/WorkState.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/nntp/ArticleDownloader.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nntp/ArticleWriter.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nntp/Decoder.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nntp/NewsServer.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nntp/NntpConnection.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nntp/ServerPool.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nntp/StatMeter.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/nserv/NntpServer.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nserv/NServFrontend.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nserv/NServMain.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nserv/NzbGenerator.cpp
+	${CMAKE_SOURCE_DIR}/daemon/nserv/YEncoder.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/Cleanup.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/DirectUnpack.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/DupeMatcher.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/ParChecker.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/ParParser.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/ParRenamer.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/PrePostProcessor.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/RarReader.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/RarRenamer.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/Rename.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/Repair.cpp
+	${CMAKE_SOURCE_DIR}/daemon/postprocess/Unpack.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/queue/DirectRenamer.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/DiskState.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/DownloadInfo.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/DupeCoordinator.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/HistoryCoordinator.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/NzbFile.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/QueueCoordinator.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/QueueEditor.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/Scanner.cpp
+	${CMAKE_SOURCE_DIR}/daemon/queue/UrlCoordinator.cpp
+
+	${CMAKE_SOURCE_DIR}/daemon/remote/BinRpc.cpp
+	${CMAKE_SOURCE_DIR}/daemon/remote/RemoteClient.cpp
+	${CMAKE_SOURCE_DIR}/daemon/remote/RemoteServer.cpp
+	${CMAKE_SOURCE_DIR}/daemon/remote/WebServer.cpp
+	${CMAKE_SOURCE_DIR}/daemon/remote/XmlRpc.cpp
+	${CMAKE_SOURCE_DIR}/daemon/remote/MessageBase.h
+
+	${CMAKE_SOURCE_DIR}/daemon/util/FileSystem.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/Log.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/NString.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/Observer.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/ScriptController.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/Service.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/Thread.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/Util.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/Json.cpp
+	${CMAKE_SOURCE_DIR}/daemon/util/Xml.cpp
+)
+
+set(WIN32_SRC
+	${CMAKE_SOURCE_DIR}/daemon/windows/StdAfx.cpp
+	${CMAKE_SOURCE_DIR}/daemon/windows/WinConsole.cpp
+	${CMAKE_SOURCE_DIR}/daemon/windows/WinService.cpp
+)
+
+if(WIN32) 
+	set(SRC ${SRC} ${WIN32_SRC})
+endif()
+
+add_executable(${PACKAGE} ${SRC})
 
 target_link_libraries(${PACKAGE} PRIVATE
 	Yencode 
