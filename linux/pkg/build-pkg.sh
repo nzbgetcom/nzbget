28ba36cee0e8f84b07e31e4ecc9ed26516c34871
phnzb <pavel@nzbget.com>
2024-04-02 12:02:50 +0300

    Refactor scripts.

---
+++ linux/pkg/build-pkg.sh
@@ -0,0 +1,125 @@
+#!/bin/sh
+
+# stop on error
+set -e
+
+# config variables
+DEB=yes
+RPM=no
+# ARCHS="i686 x86_64 armel armhf aarch64"
+ARCHS="x86_64"
+
+# prepare directories
+mkdir -p build
+rm -rf build/*
+cd build
+mkdir -p deb
+mkdir -p rpm
+
+
+INSTALLER=nzbget-latest-testing-bin-linux.run
+if [ ! -f "/tmp/$INSTALLER" ]; then
+    curl -L -o "/tmp/$INSTALLER" "https://github.com/nzbgetcom/nzbget/releases/download/testing/$INSTALLER"    
+fi
+
+# extract version
+VERSION=$(bash "/tmp/$INSTALLER" --help | grep 'Installer for' | cut -d ' ' -f 3 | sed -r 's/nzbget-//')
+RPM_VERSION=${VERSION//-/}
+
+for ARCH in $ARCHS; do
+    bash "/tmp/$INSTALLER" --arch "$ARCH" --destdir "$PWD/$ARCH" --silent    
+    case $ARCH in
+        i686)
+            DPKG_ARCH=i386
+            RPM_ARCH=i386
+            ;;
+        x86_64)
+            DPKG_ARCH=amd64
+            RPM_ARCH=x86_64
+            ;;
+        aarch64)
+            DPKG_ARCH=arm64
+            RPM_ARCH=aarch64
+            ;;
+        armhf)
+            DPKG_ARCH=armhf
+            RPM_ARCH=armv7l
+            ;;
+        armel)
+            DPKG_ARCH=armel
+            RPM_ARCH=armv6l
+            ;;
+        *)
+            DPKG_ARCH=$ARCH
+            RPM_ARCH=$ARCH
+            ;;
+    esac
+
+    # prepare files
+    CONTENTS="$PWD/$ARCH/CONTENTS/"
+    SHARE="$CONTENTS/usr/share/nzbget/"
+    SHAREDOC="$CONTENTS/usr/share/doc/nzbget/"
+    mkdir -p $CONTENTS/usr/bin
+    mkdir -p $CONTENTS/usr/share/doc/nzbget
+    mkdir -p $CONTENTS/usr/share/nzbget
+    cp $PWD/$ARCH/nzbget $CONTENTS/usr/bin
+    for DOCFILE in ChangeLog COPYING; do
+        cp $PWD/$ARCH/$DOCFILE $SHAREDOC
+    done
+    mv $PWD/$ARCH/scripts $SHARE
+    mv $PWD/$ARCH/webui $SHARE
+    cp $PWD/$ARCH/nzbget.conf $SHARE
+    cp $PWD/$ARCH/cacert.pem $SHARE
+
+    # tweak nzbget.conf
+    sed -i \
+        -e "s|^MainDir=.*|MainDir=/var/lib/nzbget/downloads|g" \
+        -e "s|^ScriptDir=.*|ScriptDir=/var/lib/nzbget/scripts|g" \
+        -e "s|^WebDir=.*|WebDir=/usr/share/nzbget/webui|g" \
+        -e "s|^TempDir=.*|TempDir=/var/lib/nzbget/tmp|g" \
+        -e "s|^ConfigTemplate=.*|ConfigTemplate=/usr/share/nzbget/nzbget.conf|g" \
+        -e "s|^UnrarCmd=.*|UnrarCmd=unrar|g" \
+        -e "s|^SevenZipCmd=.*|SevenZipCmd=7zz|g" \
+        -e "s|^CertStore=.*|CertStore=/usr/share/nzbget/cacert.pem|g" \
+        -e "s|^CertCheck=.*|CertCheck=yes|g" \
+        -e "s|^DestDir=.*|DestDir=$\{MainDir\}/completed|g" \
+        -e "s|^InterDir=.*|InterDir=$\{MainDir\}/intermediate|g" \
+        -e "s|^AuthorizedIP=.*|AuthorizedIP=127.0.0.1|g" \
+        -e 's|^UpdateCheck=.*|UpdateCheck=none|g' \
+        -e 's|^UMask=.*|UMask=0002|g' \
+        "${SHARE}nzbget.conf"
+
+    # build debian package
+    if [ "$DEB" == "yes" ]; then
+        mkdir -p $CONTENTS/DEBIAN/
+        cp ../linux/pkg/deb/DEBIAN/* "$CONTENTS/DEBIAN/"
+        eval "echo \"$(cat ../linux/pkg/deb/DEBIAN/control)\"" > "$CONTENTS/DEBIAN/control"        
+        mkdir -p "$CONTENTS/lib/systemd/system/"
+        cp ../linux/pkg/nzbget.service "$CONTENTS/lib/systemd/system/"
+        # remove unneeded files
+        find $PWD/$ARCH/ -maxdepth 1 -type f -delete        
+        fakeroot dpkg-deb --build $CONTENTS $PWD/deb/nzbget-$VERSION-$DPKG_ARCH.deb
+    fi
+
+    # build rpm package
+    if [ "$RPM" == "yes" ]; then
+        # prepare spec
+        eval "echo \"$(cat ../linux/pkg/rpm/nzbget.spec)\"" > "$ARCH/nzbget.spec"
+
+        # prepare directories
+        if [ "$ARCH" == "armel" ] || [ "$ARCH" == "armhf" ]; then
+            ARCH_PATH="arm"
+        else
+            ARCH_PATH=$RPM_ARCH
+        fi
+        RPM_SRC="$PWD/$ARCH/BUILDROOT/nzbget-$RPM_VERSION-1.$ARCH_PATH"
+        mkdir -p $RPM_SRC
+        cp -r $CONTENTS/usr/ $RPM_SRC    
+        rpmbuild --define "_topdir $PWD/$ARCH" -bb $PWD/$ARCH/nzbget.spec --target $RPM_ARCH
+        cp $PWD/$ARCH/RPMS/$RPM_ARCH/*.rpm rpm/
+    fi
+
+    rm -rf $PWD/$ARCH
+done
+
+# rm /tmp/$INSTALLER
