5d774089638052c732fa467740248756bd565cf8
phnzb <pavel@nzbget.com>
2024-04-04 11:23:26 +0300

    Refactor scripts. Add package workflow.

--- linux/pkg/build-pkg.sh
+++ linux/pkg/build-pkg.sh
@@ -3,11 +3,23 @@
 # stop on error
 set -e
 
+NZBGET_ROOT=$PWD
+
+# installer - first param
+# if empty - find installer file in artifacts dir
+INSTALLER="$1"
+if [ -z $INSTALLER ]; then
+    INSTALLER=$(find $NZBGET_ROOT/nzbget-linux-installers/ -name *linux.run 2>/dev/null) || true
+fi
+if [ -z $INSTALLER ]; then
+    echo "Cannot find linux installer file. Exitig."
+    exit 1
+fi
+
 # config variables
 DEB=yes
 RPM=no
-# ARCHS="i686 x86_64 armel armhf aarch64"
-ARCHS="x86_64"
+ARCHS="i686 x86_64 armel armhf aarch64"
 
 # prepare directories
 mkdir -p build
@@ -16,18 +28,12 @@ cd build
 mkdir -p deb
 mkdir -p rpm
 
-
-INSTALLER=nzbget-latest-testing-bin-linux.run
-if [ ! -f "/tmp/$INSTALLER" ]; then
-    curl -L -o "/tmp/$INSTALLER" "https://github.com/nzbgetcom/nzbget/releases/download/testing/$INSTALLER"    
-fi
-
 # extract version
-VERSION=$(bash "/tmp/$INSTALLER" --help | grep 'Installer for' | cut -d ' ' -f 3 | sed -r 's/nzbget-//')
+VERSION=$(bash "$INSTALLER" --help | grep 'Installer for' | cut -d ' ' -f 3 | sed -r 's/nzbget-//')
 RPM_VERSION=${VERSION//-/}
 
 for ARCH in $ARCHS; do
-    bash "/tmp/$INSTALLER" --arch "$ARCH" --destdir "$PWD/$ARCH" --silent    
+    bash "$INSTALLER" --arch "$ARCH" --destdir "$PWD/$ARCH" --silent    
     case $ARCH in
         i686)
             DPKG_ARCH=i386
@@ -92,10 +98,14 @@ for ARCH in $ARCHS; do
     # build debian package
     if [ "$DEB" == "yes" ]; then
         mkdir -p $CONTENTS/DEBIAN/
-        cp ../linux/pkg/deb/DEBIAN/* "$CONTENTS/DEBIAN/"
+        cp $NZBGET_ROOT/linux/pkg/deb/DEBIAN/* "$CONTENTS/DEBIAN/"
+        cp -r $NZBGET_ROOT/linux/pkg/deb/CONTENTS "$PWD/$ARCH/"
         eval "echo \"$(cat ../linux/pkg/deb/DEBIAN/control)\"" > "$CONTENTS/DEBIAN/control"        
         mkdir -p "$CONTENTS/lib/systemd/system/"
         cp ../linux/pkg/nzbget.service "$CONTENTS/lib/systemd/system/"
+        # fix permissions
+        chmod -R u+rwX,go+rX,go-w "$CONTENTS/lib"
+        chmod -R u+rwX,go+rX,go-w "$CONTENTS/usr"
         # remove unneeded files
         find $PWD/$ARCH/ -maxdepth 1 -type f -delete        
         fakeroot dpkg-deb --build $CONTENTS $PWD/deb/nzbget-$VERSION-$DPKG_ARCH.deb
@@ -121,5 +131,3 @@ for ARCH in $ARCHS; do
 
     rm -rf $PWD/$ARCH
 done
-
-# rm /tmp/$INSTALLER
