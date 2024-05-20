bb9e9a85d0d3670b627b7240ea11f58f212799a3
dnzbk <denis@nzbget.com>
2024-05-20 10:20:10 +0300

    revert

--- daemon/extension/ExtensionManager.h
+++ daemon/extension/ExtensionManager.h
@@ -23,7 +23,7 @@
 #include <vector>
 #include <utility>
 #include <memory>
-#include <mutex>
+#include <shared_mutex>
 #include <boost/optional.hpp>
 #include "WebDownloader.h"
 #include "Options.h"
@@ -73,7 +73,7 @@ namespace ExtensionManager
 		DeleteExtension(const Extension::Script& ext);
 
 		Extensions m_extensions;
-		mutable std::mutex m_mutex;
+		mutable std::shared_timed_mutex m_mutex;
 	};
 }
 
