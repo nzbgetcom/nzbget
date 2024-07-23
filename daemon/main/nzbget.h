d0fd4a67e422669d0949ffde49e0b6a95f30d4fc
dnzbk <denis@nzbget.com>
2024-07-23 08:51:05 +0300

    Fix: int32 overflow

--- daemon/main/nzbget.h
+++ daemon/main/nzbget.h
@@ -358,7 +358,7 @@ typedef int pid_t;
 typedef uint8_t uint8;
 typedef int16_t int16;
 typedef uint16_t uint16;
-typedef uint32_t int32;
+typedef int32_t int32;
 typedef uint32_t uint32;
 typedef int64_t int64;
 typedef uint64_t uint64;
