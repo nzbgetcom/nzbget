a72db2b0d00afdf5f0422dbade3e0b1ec2ca6cdd
phnzb <pavel@nzbget.com>
2024-12-02 13:50:43 +0300

    remove test file

--- test.sh
+++
@@ -1,8 +0,0 @@
-GITHUB_REF_NAME="feature/github-runners-build"
-cd build
-SUFFIX="-${GITHUB_REF_NAME/\//-}-bin-linux"
-for FILE in *.run; do
-    [ -f $FILE ] || continue
-    NEW_FILE=${FILE/-bin-linux/$SUFFIX}
-    mv $FILE $NEW_FILE
-done
