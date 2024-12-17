68e5ea3da3cc6ee7e87aa691c5b2d57afe783c2d
dnzbk <denis@nzbget.com>
2024-12-17 12:11:55 +0300

    test

--- cmake/par2-turbo.cmake
+++ cmake/par2-turbo.cmake
@@ -28,7 +28,7 @@ ExternalProject_add(
 	par2-turbo
 	PREFIX			par2-turbo
 	GIT_REPOSITORY	https://github.com/nzbgetcom/par2cmdline-turbo.git
-	GIT_TAG			v1.2.0-nzbget
+	GIT_TAG			nzbget
 	TLS_VERIFY		TRUE
 	GIT_SHALLOW		TRUE
 	GIT_PROGRESS	TRUE
