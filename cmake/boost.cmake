a0e27026d9d606233104eb1ea637a0612e39e1ce
Denis <146707790+dnzbk@users.noreply.github.com>
2024-12-26 14:45:43 +0300

    Fix: build using CMake Ninja generator and the tests build failure with -DDISABLE_PARCHECK=yes (#481)
    - fixed build using CMake Ninja generator
    - fixed tests build failure with  -DDISABLE_PARCHECK=yes

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,19 +1,22 @@
-set(ROOT ${CMAKE_BINARY_DIR}/boost/)
+set(BOOST_ROOT ${CMAKE_BINARY_DIR}/boost/src)
+set(BOOST_LIBS ${BOOST_ROOT}/boost/stage/lib/libboost_json.a)
 
 ExternalProject_add(
 	boost
 	PREFIX boost
 	URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.xz
-	TLS_VERIFY TRUE
+	TLS_VERIFY 		TRUE
 	BUILD_IN_SOURCE TRUE
-	GIT_SHALLOW TRUE
+	GIT_SHALLOW 	TRUE
+	GIT_PROGRESS	TRUE
 	DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-	CONFIGURE_COMMAND ${ROOT}src/boost/bootstrap.sh 
-		--with-libraries=json 
-		--prefix=${ROOT}build
-	BUILD_COMMAND ${ROOT}src/boost/b2 link=static
-	INSTALL_COMMAND ${ROOT}src/boost/b2 install
+	BUILD_BYPRODUCTS  ${BOOST_LIBS}
+	CONFIGURE_COMMAND ${BOOST_ROOT}/boost/bootstrap.sh 
+					--with-libraries=json 
+					--prefix=${BOOST_ROOT}/build
+	BUILD_COMMAND ${BOOST_ROOT}/boost/b2 link=static
+	INSTALL_COMMAND ""
 )
 
-set(LIBS ${LIBS} ${ROOT}build/lib/libboost_json.a)
-set(INCLUDES ${INCLUDES} ${ROOT}build/include/)
+set(LIBS ${LIBS} ${BOOST_LIBS})
+set(INCLUDES ${INCLUDES} ${BOOST_ROOT}/boost)
