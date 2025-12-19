f4a73d8a6b67d91dfb16bcd88f4f4a30b031edc4
dnzbk <denis@nzbget.com>
2025-12-19 11:05:30 +0300

    Fix: Script ordering

--- daemon/queue/Scanner.cpp
+++ daemon/queue/Scanner.cpp
@@ -25,7 +25,6 @@
 #include "WorkState.h"
 #include "Log.h"
 #include "QueueCoordinator.h"
-#include "HistoryCoordinator.h"
 #include "ScanScript.h"
 #include "Util.h"
 #include "FileSystem.h"
@@ -451,21 +450,22 @@ void Scanner::InitPPParameters(const char* category, NzbParameterList* parameter
 	if (!Util::EmptyStr(extensions))
 	{
 		// create pp-parameter for each post-processing or queue- script
-		Tokenizer tok(extensions, ",;");
-		while (const char* scriptName = tok.Next())
-		{
-			g_ExtensionManager->ForEach([&](auto script)
+		g_ExtensionManager->ForEach(
+			[&](auto script)
+			{
+				Tokenizer tok(extensions, ",;");
+				while (const char* scriptName = tok.Next())
 				{
+					if (strcmp(scriptName, script->GetName()) != 0) continue;
+
 					BString<1024> paramName("%s:", scriptName);
 					if ((script->GetPostScript() || script->GetQueueScript()) &&
-						!parameters->Find(paramName) &&
-						strcmp(scriptName, script->GetName()) == 0)
+						!parameters->Find(paramName))
 					{
 						parameters->SetParameter(paramName, "yes");
 					}
 				}
-			);
-		}
+			});
 	}
 }
 
