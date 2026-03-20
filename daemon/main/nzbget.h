a594a43e1c3f4f0aef3bca2dc7aaecdbc4547a8c
dnzbk <denis@nzbget.com>
2026-03-20 18:14:46 +0300

    Fix: Ensure build with GCC 15+ by explicitly including <cstdint>

--- daemon/main/nzbget.h
+++ daemon/main/nzbget.h
@@ -147,6 +147,7 @@
 #include <time.h>
 #include <ctype.h>
 #include <inttypes.h>
+#include <cstdint>
 
 #include <string>
 #include <string_view>
@@ -291,23 +292,13 @@
 #define SHUT_RDWR 2
 #endif
 
-#ifdef HAVE_STDINT_H
-typedef uint8_t uint8;
-typedef int16_t int16;
-typedef uint16_t uint16;
-typedef int32_t int32;
-typedef uint32_t uint32;
-typedef int64_t int64;
-typedef uint64_t uint64;
-#else
-typedef unsigned char uint8;
-typedef signed short int16;
-typedef unsigned short uint16;
-typedef signed int int32;
-typedef unsigned int uint32;
-typedef signed long long int64;
-typedef unsigned long long uint64;
-#endif
+using uint8 = uint8_t;
+using int16 = int16_t;
+using uint16 = uint16_t;
+using int32 = int32_t;
+using uint32 = uint32_t;
+using int64 = int64_t;
+using uint64 = uint64_t;
 
 #ifndef PRId64
 #define PRId64 "lld"
