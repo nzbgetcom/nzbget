nzbgetcom/nzbget

commit 488e50d3fddb95d108b0a3e9d2bf3d447673f149

Fix errors when renaming NZB during download, avoid 0-byte file and improve hardlink cleanup (#768)
 - Skips file completion for 0-byte/empty files (0 successful articles)
 - Fixes "No such file or directory" errors when renaming an ongoing NZB download by checking if files exist before moving them
 - Automatically removes intermediate hardlink directories
 - Persists hardlink path in DiskState (bump queue version to 63)

daemon/postprocess/Cleanup.cpp
@@ -2,7 +2,7 @@
  *  This file is part of nzbget. See <https://nzbget.com>.
  *
  *  Copyright (C) 2013-2016 Andrey Prygunkov <hugbug@users.sourceforge.net>
- *  Copyright (C) 2025 Denis <denis@nzbget.com>
+ *  Copyright (C) 2025-2026 Denis <denis@nzbget.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,12 +19,12 @@
  */
 
 
+#include "DownloadInfo.h"
 #include "nzbget.h"
 #include "Cleanup.h"
 #include "Log.h"
 #include "Util.h"
 #include "FileSystem.h"
-#include "ParParser.h"
 #include "Options.h"
 
 void MoveController::StartJob(PostInfo* postInfo)
@@ -59,6 +59,8 @@ void MoveController::Run()
 	PrintMessage(Message::mkInfo, "Moving completed files for %s", nzbName.c_str());
 
 	bool ok = MoveFiles();
+
+	RemoveStaleHardlinks(*m_postInfo->GetNzbInfo(), m_destDir);
 
 	infoName[0] = 'M'; // uppercase
 
@@ -203,6 +205,20 @@ void CleanupController::Run()
 	}
 
 	m_postInfo->SetWorking(false);
+}
+
+void MoveController::RemoveStaleHardlinks(NzbInfo& nzbInfo, std::string_view destDir)
+{
+	const auto& hardLinkPath = nzbInfo.GetHardLinkPath();
+	if (hardLinkPath.empty() || hardLinkPath == destDir) return;
+
+	fs::error_code ec;
+	const auto path = fs::u8path(hardLinkPath);
+	fs::remove_all(path, ec);
+	if (ec)
+	{
+		PrintMessage(Message::mkError, "Could not remove old hardlink directory: %s", ec.message().c_str());
+	}
 }
 
 bool CleanupController::Cleanup(const char* destDir, bool *deleted)

daemon/nntp/ArticleWriter.h
@@ -2,7 +2,7 @@
  *  This file is part of nzbget. See <https://nzbget.com>.
  *
  *  Copyright (C) 2014-2019 Andrey Prygunkov <hugbug@users.sourceforge.net>
- *  Copyright (C) 2024-2025 Denis <denis@nzbget.com>
+ *  Copyright (C) 2024-2026 Denis <denis@nzbget.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -23,6 +23,7 @@
 #define ARTICLEWRITER_H
 
 #include <atomic>
+#include <optional>
 #include <string>
 #include "NString.h"
 #include "DownloadInfo.h"
@@ -51,6 +52,11 @@ private:
 class ArticleWriter
 {
 public:
+	struct OutputPaths 
+	{
+	    std::string finalPath;
+	    std::string tempPath;
+	};
 	void SetInfoName(const char* infoName) { m_infoName = infoName ? infoName : ""; }
 	void SetInfoName(std::string infoName) { m_infoName = std::move(infoName); }
 	void SetFileInfo(FileInfo* fileInfo) { m_fileInfo = fileInfo; }
@@ -60,24 +66,36 @@ public:
 	bool Write(char* buffer, int len);
 	void Finish(bool success);
 	bool GetDuplicate() { return m_duplicate; }
+	void LogStartMessage(std::string_view infoFilename, bool directWrite, bool cached);
+	std::optional<OutputPaths> SetupOutputFile(DiskFile &outfile,
+											std::string_view destDir,
+											std::string_view filename,
+											std::string_view infoFilename,
+											bool directWrite,
+											bool cached);
+	uint32 ProcessArticles(DiskFile& outfile,
+						std::string_view infoFilename,
+						std::string_view finalOutputPath,
+						bool directWrite,
+						bool cached);
+	void CommitDiskFile(DiskFile& outfile,
+						std::string_view tempDestPath,
+						std::string_view finalOutputPath,
+						bool directWrite);
+	void CleanupOldData(bool directWrite,
+						std::string_view nzbDestDir,
+						std::string_view finalOutputPath);
+	void ReportCompletionStatus(std::string_view infoFilename);
+	void HandlePostProcessing(uint32 crc, 
+							std::string_view currentPath, 
+							std::string_view originalFilename, 
+							std::string_view nzbDestDir);
 	void CompleteFileParts();
 	static bool MoveCompletedFiles(NzbInfo* nzbInfo, const char* oldDestDir);
 	void FlushCache();
 
 private:
 	bool GetSkipDiskWrite();
-
-	/**
-	 *  @brief Renames the temporary `.out` output file to its original filename.
-	 *  
-	 * 	This is necessary when Direct/Par renaming is disabled or the download fails due to health checks.
-	 *  Or there are no par files at all.
-	 *  No action is taken if the filename matches the current filename.
-	 *
-	 * @param filename The desired final filename (without path).
-	 * @param destDir  The destination directory for the file.
-	 */
-	void RenameOutputFile(const std::string& filename, const std::string& destDir);
 
 	FileInfo* m_fileInfo;
 	ArticleInfo* m_articleInfo;

daemon/postprocess/Cleanup.h
@@ -2,7 +2,7 @@
  *  This file is part of nzbget. See <https://nzbget.com>.
  *
  *  Copyright (C) 2013-2016 Andrey Prygunkov <hugbug@users.sourceforge.net>
- *  Copyright (C) 2025 Denis <denis@nzbget.com>
+ *  Copyright (C) 2025-2026 Denis <denis@nzbget.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -32,11 +32,11 @@
 class MoveController : public Thread, public ScriptController
 {
 public:
-	virtual void Run();
+	void Run() override;
 	static void StartJob(PostInfo* postInfo);
 
 protected:
-	virtual void AddMessage(Message::EKind kind, const char* text);
+	void AddMessage(Message::EKind kind, const char* text) override;
 
 private:
 	PostInfo* m_postInfo;
@@ -45,16 +45,17 @@ private:
 
 	bool MoveFiles();
 	void MoveFiles(const std::string& src, const std::string& dest, bool& isOk);
+	void RemoveStaleHardlinks(NzbInfo& nzbInfo, std::string_view destDir);
 };
 
 class CleanupController : public Thread, public ScriptController
 {
 public:
-	virtual void Run();
+	void Run() override;
 	static void StartJob(PostInfo* postInfo);
 
 protected:
-	virtual void AddMessage(Message::EKind kind, const char* text);
+	void AddMessage(Message::EKind kind, const char* text) override;
 
 private:
 	PostInfo* m_postInfo;

daemon/queue/DirectRenamer.cpp
@@ -2,7 +2,7 @@
  *  This file is part of nzbget. See <https://nzbget.com>.
  *
  *  Copyright (C) 2017-2019 Andrey Prygunkov <hugbug@users.sourceforge.net>
- *  Copyright (C) 2025 Denis <denis@nzbget.com>
+ *  Copyright (C) 2025-2026 Denis <denis@nzbget.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -440,25 +440,37 @@ int DirectRenamer::RenameFilesInProgress(NzbInfo* nzbInfo, FileHashList* parHash
 		// Create hardlink and save the path in fileInfo
 		if (g_Options->GetHardLinking() && !Util::MatchFileExt(newName.c_str(), g_Options->GetHardLinkingIgnoreExt(), ","))
 		{
-			const std::string nzbFinalDir = fileInfo->GetNzbInfo()->BuildFinalDirName().Str();
-			const std::string finalOutputFilename = nzbFinalDir + PATH_SEPARATOR + newName;
-
-			nzbInfo->PrintMessage(Message::mkInfo,
-				"HardLinking in-progress file %s to %s",
-				oldOutputFilename.c_str(), finalOutputFilename.c_str()
-			);
-
-			CString errmsg;
-			if (FileSystem::CreateHardLink(oldOutputFilename.c_str(), finalOutputFilename.c_str(), errmsg))
+			fs::error_code ec;
+			if (!fs::exists(oldOutputFilename, ec))
 			{
-				fileInfo->SetHardLinkPath(finalOutputFilename);
+				nzbInfo->PrintMessage(Message::mkDetail,
+					"Skipping hardlink for %s: file not assembled yet",
+					oldOutputFilename.c_str()
+				);
 			}
 			else
 			{
-				nzbInfo->PrintMessage(Message::mkError,
-					"Could not create hardlink %s to %s: %s",
-					oldOutputFilename.c_str(), finalOutputFilename.c_str(), *errmsg
+				std::string nzbFinalDir = fileInfo->GetNzbInfo()->BuildFinalDirName().Str();
+				std::string finalOutputFilename = nzbFinalDir + PATH_SEPARATOR + newName;
+
+				nzbInfo->PrintMessage(Message::mkInfo,
+					"HardLinking in-progress file %s to %s",
+					oldOutputFilename.c_str(), finalOutputFilename.c_str()
 				);
+
+				CString errmsg;
+				if (FileSystem::CreateHardLink(oldOutputFilename.c_str(), finalOutputFilename.c_str(), errmsg))
+				{
+					fileInfo->SetHardLinkPath(std::move(finalOutputFilename));
+					nzbInfo->SetHardLinkPath(std::move(nzbFinalDir));
+				}
+				else
+				{
+					nzbInfo->PrintMessage(Message::mkError,
+						"Could not create hardlink %s to %s: %s",
+						oldOutputFilename.c_str(), finalOutputFilename.c_str(), *errmsg
+					);
+				}				
 			}
 		}
 
@@ -513,21 +525,32 @@ int DirectRenamer::RenameCompletedFiles(NzbInfo* nzbInfo, FileHashList* parHashe
 
 		if (!Util::EmptyStr(g_Options->GetInterDir()) && g_Options->GetHardLinking() && !Util::MatchFileExt(newName.c_str(), g_Options->GetHardLinkingIgnoreExt(), ","))
 		{
-			const std::string finalDir = nzbInfo->BuildFinalDirName().Str();
-			const std::string finalOutputFilename = finalDir + PATH_SEPARATOR + newName;
-
-			nzbInfo->PrintMessage(Message::mkInfo,
-				"HardLinking completed file %s to %s",
-				oldOutputFilename.c_str(), finalOutputFilename.c_str()
-			);
-
-			CString errmsg;
-			if (!FileSystem::CreateHardLink(oldOutputFilename.c_str(), finalOutputFilename.c_str(), errmsg))
+			fs::error_code ec;
+			if (!fs::exists(oldOutputFilename, ec))
 			{
-				nzbInfo->PrintMessage(Message::mkError,
-					"Could not create hardlink %s to %s: %s",
-					oldOutputFilename.c_str(), finalOutputFilename.c_str(), *errmsg
+				nzbInfo->PrintMessage(Message::mkDetail,
+					"Skipping hardlink for %s: file not assembled yet",
+					oldOutputFilename.c_str()
 				);
+			}
+			else
+			{
+				const std::string finalDir = nzbInfo->BuildFinalDirName().Str();
+				const std::string finalOutputFilename = finalDir + PATH_SEPARATOR + newName;
+
+				nzbInfo->PrintMessage(Message::mkInfo,
+					"HardLinking completed file %s to %s",
+					oldOutputFilename.c_str(), finalOutputFilename.c_str()
+				);
+
+				CString errmsg;
+				if (!FileSystem::CreateHardLink(oldOutputFilename.c_str(), finalOutputFilename.c_str(), errmsg))
+				{
+					nzbInfo->PrintMessage(Message::mkError,
+						"Could not create hardlink %s to %s: %s",
+						oldOutputFilename.c_str(), finalOutputFilename.c_str(), *errmsg
+					);
+				}				
 			}
 		}
 

daemon/queue/DiskState.cpp
@@ -2,7 +2,7 @@
  *  This file is part of nzbget. See <https://nzbget.com>.
  *
  *  Copyright (C) 2007-2019 Andrey Prygunkov <hugbug@users.sourceforge.net>
- *  Copyright (C) 2024-2025 Denis <denis@nzbget.com>
+ *  Copyright (C) 2024-2026 Denis <denis@nzbget.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@
 #include "FileSystem.h"
 
 static const char* FORMATVERSION_SIGNATURE = "nzbget diskstate file version ";
-const int DISKSTATE_QUEUE_VERSION = 62;
+const int DISKSTATE_QUEUE_VERSION = 63;
 const int DISKSTATE_FILE_VERSION = 7;
 const int DISKSTATE_STATS_VERSION = 4;
 const int DISKSTATE_FEEDS_VERSION = 3;
@@ -543,6 +543,7 @@ void DiskState::SaveNzbInfo(NzbInfo* nzbInfo, StateDiskFile& outfile)
 	outfile.PrintLine("%s", nzbInfo->GetFilename());
 	outfile.PrintLine("%s", nzbInfo->GetDestDir());
 	outfile.PrintLine("%s", nzbInfo->GetFinalDir());
+	outfile.PrintLine("%s", nzbInfo->GetHardLinkPath().c_str());
 	outfile.PrintLine("%s", nzbInfo->GetQueuedFilename());
 	outfile.PrintLine("%s", nzbInfo->GetName());
 	outfile.PrintLine("%s", nzbInfo->GetCategory());
@@ -664,6 +665,12 @@ bool DiskState::LoadNzbInfo(NzbInfo* nzbInfo, Servers* servers, StateDiskFile& i
 
 	if (!infile.ReadLine(buf, sizeof(buf))) goto error;
 	nzbInfo->SetFinalDir(buf);
+
+	if (formatVersion >= 63) 
+	{
+		if (!infile.ReadLine(buf, sizeof(buf))) goto error;
+		nzbInfo->SetHardLinkPath(buf);
+	}
 
 	if (!infile.ReadLine(buf, sizeof(buf))) goto error;
 	nzbInfo->SetQueuedFilename(buf);

daemon/queue/DownloadInfo.h
@@ -3,7 +3,7 @@
  *
  *  Copyright (C) 2004 Sven Henkel <sidddy@users.sourceforge.net>
  *  Copyright (C) 2007-2019 Andrey Prygunkov <hugbug@users.sourceforge.net>
- *  Copyright (C) 2024-2025 Denis <denis@nzbget.com>
+ *  Copyright (C) 2024-2026 Denis <denis@nzbget.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
 #define DOWNLOADINFO_H
 
 #include <atomic>
-#include "NewsServer.h"
+#include <string>
 #include "NString.h"
 #include "Container.h"
 #include "Observer.h"
@@ -495,6 +495,8 @@ public:
 	const char* GetCategory() { return m_category; }
 	void SetCategory(const char* category) { m_category = category; }
 	const char* GetName() { return m_name; }
+	void SetHardLinkPath(std::string hardLinkPath) { m_hardLinkPath = std::move(hardLinkPath); }
+	const std::string& GetHardLinkPath() const { return m_hardLinkPath; }
 	void SetName(const char* name) { m_name = name; }
 	int GetFileCount() { return m_fileCount; }
 	void SetFileCount(int fileCount) { m_fileCount = fileCount; }
@@ -687,6 +689,7 @@ private:
 	CString m_destDir = "";
 	CString m_finalDir = "";
 	CString m_category = "";
+	std::string m_hardLinkPath;
 	int m_fileCount = 0;
 	int m_parkedFileCount = 0;
 	int64 m_size = 0;

daemon/util/FileSystem.h
@@ -88,7 +88,7 @@ inline path make_unique_filename()
 inline fs::path make_unique_filename(const fs::path& targetPath)
 {
 	fs::error_code ec;
-	if (!fs::exists(targetPath, ec) && !ec) 
+	if (!fs::exists(targetPath, ec) && !ec)
 	{
 		return targetPath;
 	}
@@ -96,10 +96,10 @@ inline fs::path make_unique_filename(const fs::path& targetPath)
 	const fs::path baseDir = targetPath.parent_path();
 	const std::string stem = fs::u8string(targetPath.stem());
 	const std::string ext = fs::u8string(targetPath.extension());
-	
+
 	int counter = 1;
 	fs::path uniquePath;
-	do 
+	do
 	{
 		uniquePath = baseDir / (stem + " (" + std::to_string(counter++) + ")" + ext);
 	} while (fs::exists(uniquePath, ec) && !ec);

commit bc527428f07a9367d00eb60833a139d6dba189b1

Fix missing nzbget.h include order in Cleanup.cpp
Include nzbget.h before other headers in Cleanup.cpp to ensure macros
and types are properly declared before use. This is not caught when
building with precompiled headers, as nzbget.h is implicitly available
to all translation units. Broken since 488e50d3.

See also 063e1cd82ee719b18a1dae40f65c0971de44edef which fixed the same
issue in UrlCoordinator.cpp and PathsValidator.cpp.

Without this fix, compilation fails with errors like:
```
daemon/util/NString.h:34:49: error: expected ';' at end of member declaration [-Wtemplate-body]
   34 |         explicit BString(const char* format, ...) PRINTF_SYNTAX(2);
      |                                                 ^
      |                                                  ;
daemon/util/NString.h:34:65: error: expected identifier before numeric constant [-Wtemplate-body]
```

daemon/postprocess/Cleanup.cpp
@@ -19,8 +19,8 @@
  */
 
 
-#include "DownloadInfo.h"
 #include "nzbget.h"
+#include "DownloadInfo.h"
 #include "Cleanup.h"
 #include "Log.h"
 #include "Util.h"