3d9db1197c1f99420644b10f8fad9f9cf7a6bb71
XBMC 4 Lyfe <273732874+xbmc4lyfe@users.noreply.github.com>
2026-07-27 08:19:27 -0500

    Fix Download Remaining Files (From History) Doesn't work for Health failed files (#870)

--- daemon/queue/HistoryCoordinator.cpp
+++ daemon/queue/HistoryCoordinator.cpp
@@ -518,6 +518,7 @@ void HistoryCoordinator::HistoryRedownload(DownloadQueue* downloadQueue, History
 	nzbInfo->SetRarRenameStatus(NzbInfo::rsNone);
 	nzbInfo->SetDirectRenameStatus(NzbInfo::tsNone);
 	nzbInfo->SetDirectUnpackStatus(NzbInfo::nsNone);
+	nzbInfo->SetHealthPaused(false);
 	nzbInfo->SetDownloadedSize(0);
 	nzbInfo->SetDownloadSec(0);
 	nzbInfo->SetPostTotalSec(0);
@@ -660,6 +661,12 @@ void HistoryCoordinator::HistoryRetry(DownloadQueue* downloadQueue, HistoryList:
 	}
 
 	nzbInfo->UpdateCurrentStats();
+	if (!resetFailed && !reprocess)
+	{
+		// The user explicitly requested the remaining files; do not park the
+		// download again because of the health failure being overridden.
+		nzbInfo->SetHealthPaused(true);
+	}
 
 	MoveToQueue(downloadQueue, itHistory, historyInfo, reprocess);
 
